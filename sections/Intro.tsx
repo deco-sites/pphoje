d566e5a0a81ce20d90a88893074c61e0b8350469
SolracSiul <crluis521@gmail.com>
2023-05-27 23:40:22 -0300

    wip: card-c

--- sections/Intro.tsx
+++ sections/Intro.tsx
@@ -1,74 +1,26 @@
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Card  from '../components/ui/Card.tsx'
+import { Programacao } from '../configssaojoao/Programacao.ts'
 
-export interface IDate {
-  /**
-   * @format date-time
-   */
-  dia: string;
+export interface Props{
+  prog?: Programacao[];
 }
-export interface Artist {
-  mobile?: LiveImage;
-  name: string;
-  bio: string;
-  date: IDate;
-}
-export interface Props {
-  performers: Artist[];
-}
-function formatarData(data: string) {
-  const partes = data.split(/[-T.:Z]/);
-  const mes = [
-    "janeiro",
-    "fevereiro",
-    "março",
-    "abril",
-    "maio",
-    "junho",
-    "julho",
-    "agosto",
-    "setembro",
-    "outubro",
-    "novembro",
-    "dezembro",
-  ][parseInt(partes[1], 10)];
-  return [partes[2], ` de ${mes} `, partes[3] + ":" + partes[4]];
-}
-function Intro({ performers }: Props) {
-  {
-    console.log("hora" + performers.map((x) => x.date));
-  }
+
+function Intro({prog}: Props) {
+  const today = prog && prog.flatMap((item) =>item.days.map((day) => day.referenceDate));
   return (
-    <div>
-      <h1 class="mt-4">Abertura do maior São João do Mundo</h1>
-      <div class="mx-4">
-        {performers.map((artist, index) => (
-          <div class="flex items-center border-b-2 border-gray-400 border-solid py-2">
-            <div>
-              <img
-                src={artist.mobile}
-                alt=""
-                width={60}
-                height={60}
-                class="rounded-full w-[62px] h-[62px] bg-red-card opacity-95"
-              />
-            </div>
-            <div>
-              <h2 class="ml-4">{artist.name}</h2>
-            </div>
-            <div class="ml-auto my-auto self-end">
-              <h2 class="">{formatarData(artist.date.dia)[2]}</h2>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div class="flex flex-col">
+      <h1 class="text-center font-bold text-btn-label">Programação de Hoje</h1>
+      <h2 class="text-center font-bold ">{today}</h2>
+      <Card prog={prog}/>
       <div class="flex justify-center items-center mt-2">
         <a href="#lineUp">
-          <button class="bg-btn-default text-white py-2 px-2 rounded-[5px]">
+          <button class="btn py-2 px-2 rounded-[5px]">
             Programação completa
           </button>
         </a>
         <a href="#search">
-          <button class="ml-2 bg-btn-default text-white py-2 px-2 rounded-[5px]">
+          <button class="btn ml-2 py-2 px-2 rounded-[5px]">
             LineUp
           </button>
         </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import daisyui from "daisyui";
 
-console.log({ daisyui });
+// console.log({ daisyui });
 export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: { 
+    extend:{
+      colors: {
+        "card-date": "#EB8A90",
+        "card-name": "#120D26",
+        "card-local": "#747688",
+        "btn-label": "#6B21A8"
+        
+      },
+    },
+    container: { center: true } 
+  },
 };
