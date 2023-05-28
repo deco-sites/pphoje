2602658c5f33cfe1d2cc4a8f23576c8524d30792
Lucis <luciannojunior@gmail.com>
2023-05-28 00:43:41 -0300

    Busca da programação (#16)
    * whs
    
    * Accepting Programacao
    
    * Programacao
    
    * Improve card

---
+++ islands/ProgramacaoCompleta.tsx
@@ -0,0 +1,85 @@
+import type { Programacao } from "../configssaojoao/Programacao.ts";
+import Card from "../components/ui/Card.tsx";
+import { useState } from "preact/hooks";
+
+export interface Props {
+  programacao?: Programacao;
+}
+
+function formatarData(data: string) {
+  const partes = data.split(/[-T.:Z]/);
+  const mes = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+  ][parseInt(partes[1], 10)];
+  return [partes[2], ` de ${mes} `, partes[3] + ":" + partes[4]];
+}
+
+function ProgramacaoCompleta({ programacao }: Props) {
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [selectedPerformer, setSelectedPerformer] = useState<any | undefined>(
+    undefined
+  );
+  const [dialogShow, setDialogShow] = useState(false);
+  const [busca, setBusca] = useState("");
+
+  function openDialog(index: number) {
+    const modal = document.querySelector("[data-modal-2]") as HTMLDialogElement;
+    document.body.style.overflowY = "hidden";
+    const buscado = programacao && programacao.days[0];
+    setSelectedPerformer(buscado);
+    setDialogShow(true);
+    modal.showModal();
+  }
+  function closeDialog() {
+    const modal = document.querySelector("[data-modal-2]") as HTMLDialogElement;
+    setDialogShow(false);
+    document.body.style.overflowY = "auto";
+    modal.close();
+  }
+  function handleMouseOver(index: number) {
+    setHoveredIndex(index);
+  }
+
+  function handleMouseOut() {
+    setHoveredIndex(-1);
+  }
+  function handleChange(e: Event) {
+    const inputElement = e.target as HTMLInputElement;
+    setBusca(inputElement.value);
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <span class="text-xl font-bold mt-4" id="search">
+        Programação Completa
+      </span>
+
+      <input
+        type="text"
+        class="input input-bordered w-11/12"
+        placeholder="Busque por artista ou dia"
+      />
+      <div
+        class="flex w-11/12 p-2 flex-col overflow-scroll scroll-smooth mt-4 rounded-3xl border h-[500px]"
+        style="box-shadow: inset 0 10px 10px -10px rgba(0, 0, 0, 0.5), inset 0 -10px 10px -10px rgba(0, 0, 0, 0.5);"
+      >
+        {[1, 2, 3, 4].map((a) => (
+          <Card />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProgramacaoCompleta;
--- sections/Festas.tsx
+++ sections/Festas.tsx
@@ -1,44 +1,12 @@
-import Programacao from "../islands/Programacao.tsx";
-
-export interface IDate {
-  /**
-   * @format date-time
-   */
-  dia: string;
-}
-export interface IParty {
-  id?: string;
-  name?: string;
-  houses?: Array<IHouse>;
-}
-
-export interface IHouse {
-  name?: string;
-  days: Array<IDay>;
-}
-
-export interface IDay {
-  date: IDate;
-  artists?: Array<IArtist>;
-}
-
-export interface IArtist {
-  name?: string;
-  bio?: string;
-  socialMedia?: Array<string>;
-  showDateTime: IDate;
-}
+import type { Programacao } from "../configssaojoao/Programacao.ts";
+import ProgramacaoCompleta from "../islands/ProgramacaoCompleta.tsx";
 
 export interface Props {
-  festas?: Array<IParty>;
+  programacao: Programacao;
 }
 
-function Festas({ festas }: Props) {
-  return (
-    <div>
-      <Programacao festas={festas} />
-    </div>
-  );
+function Festas({ programacao }: Props) {
+  return <ProgramacaoCompleta programacao={programacao} />;
 }
 
 export default Festas;
